43bce01f960e04fa6c49e035af44529aecc030f1
Gonzales <anggonzalesca@upt.pe>
2020-01-14 11:27:44 -0500

    Reemplazando el activity main

---
+++ app/src/main/java/com/example/u1tema1kotlin/layout2.kt
@@ -0,0 +1,12 @@
+package com.example.u1tema1kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class layout2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout2)
+    }
+}
