f7376f0ea64dbf3072dacda33ce6ad47a9965b15
Gonzales <anggonzalesca@upt.pe>
2020-01-14 11:37:00 -0500

    Creando una nueva actividad3 y cambio en el Manifest

---
+++ app/src/main/java/com/example/u1tema1kotlin/actividad3.kt
@@ -0,0 +1,12 @@
+package com.example.u1tema1kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class actividad3 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actividad3)
+    }
+}
