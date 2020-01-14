096e3c273d6143083ec4afdcf647f81d0c3e3a24
Gonzales <anggonzalesca@upt.pe>
2020-01-14 11:43:29 -0500

    Agregando Mi Spinner y modificando Actividad3

--- app/src/main/java/com/example/u1tema1kotlin/actividad3.kt
+++ app/src/main/java/com/example/u1tema1kotlin/actividad3.kt
@@ -1,7 +1,12 @@
 package com.example.u1tema1kotlin
 
+import android.content.DialogInterface
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class actividad3 : AppCompatActivity() {
 
@@ -9,4 +14,54 @@ class actividad3 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_actividad3)
     }
+
+    fun actividad2(view: View) {
+        startActivity(Intent(this, Main2Activity::class.java))
+    }
+
+    fun mispinner(view: View) {
+        startActivity(Intent(this, mi_spinner::class.java))
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show()
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show()
+        super.onDestroy()
+    }
+
+    fun dialogo(view: View) {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Este es mi dialogo")
+                .setTitle("Mi primer dialogo...")
+                .setCancelable(false)
+                .setNeutralButton("Aceptar"
+                ) { dialog, id -> dialog.cancel() }
+        val alert = builder.create()
+        alert.show()
+    }
+
 }
---
+++ app/src/main/java/com/example/u1tema1kotlin/mi_spinner.kt
@@ -0,0 +1,12 @@
+package com.example.u1tema1kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class mi_spinner : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mi_spinner)
+    }
+}
